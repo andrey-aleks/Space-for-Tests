518f01a037348bc4ca566a752056f42ff7591843
andrey-aleks <andreyalekseev@croc.ru>
2021-05-03 00:07:24 +0300

    FIX: добавил в скрипте поиск всех объектов со сцены

--- Assets/Scripts/MaterialAdder.cs
+++ Assets/Scripts/MaterialAdder.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = System.Object;
 
 public class MaterialAdder : EditorWindow
 {
     public MonoScript script;
     public Material newMaterial;
-    
-    private List<GameObject> _teleportObjects;
+
+    private List<object> _teleportObjects;
+
     [MenuItem("Tools/MaterialAdder")]
     public static void ShowWindow()
     {
@@ -24,35 +27,65 @@ public class MaterialAdder : EditorWindow
         script = (MonoScript) EditorGUILayout.ObjectField("Script", script, typeof(MonoScript));
         if (GUILayout.Button("Add"))
         {
-            AddMaterial();
+           // FindObjects();
+           FindAllActiveObjects();
         }
     }
 
-    private void AddMaterial()
+    private void FindAllActiveObjects()
+    {
+        GameObject[] objects = FindObjectsOfType<GameObject>();
+        foreach (var _object in objects)
+        {
+            if (_object.activeInHierarchy)
+            {
+                Debug.Log(_object.name);
+            }
+        }
+    }
+
+    /*    private void FindObjects()
     {
         var info = new DirectoryInfo("D:/ProjectsUnity/TestingURP/Assets/Models");
         var fileInfo = info.GetFiles();
+        for (int i = 0; i < fileInfo.Length; i++)
+        {
+            _teleportObjects.Add(fileInfo.GetValue(i));
+        }
         foreach (var file in fileInfo)
         {
             var fileName = file.Name;
-            if (fileName.Split('_').Last().Contains("Tel") && (!fileName.Contains(".meta")))
+            if (!fileName.Split('_').Last().Contains("Tel") || (fileName.Contains(".meta")))
             {
-                Debug.Log(fileName);
-                fileName = fileName.Substring(0, fileName.Length - 4);
-                Debug.Log(fileName);
-                GameObject gameObject = GameObject.Find(fileName);
-                if (gameObject != null)
-                {
-                    if (gameObject.GetComponent(script.GetClass()) != null ||
-                        gameObject.GetComponent<Renderer>().material == newMaterial)
-                    {
-                        return;
-                    }
-                    gameObject.GetComponent<MeshRenderer>().material = newMaterial;
-                    gameObject.AddComponent(script.GetClass());
-
-                }
+                _teleportObjects.Remove(file);
             }
         }
-        }
+        AddComponents();
     }
+
+    private void AddComponents()
+    {
+        foreach (var _object in _teleportObjects)
+        {
+            
+        }
+        Debug.Log(fileName);
+        fileName = fileName.Substring(0, fileName.Length - 4);
+        Debug.Log(fileName);
+        GameObject gameObject = GameObject.Find(fileName);
+        if (gameObject != null)
+        {
+            if (gameObject.GetComponent(script.GetClass()) != null ||
+                gameObject.GetComponent<Renderer>().material == newMaterial)
+            {
+                return;
+            }
+
+            gameObject.GetComponent<MeshRenderer>().material = newMaterial;
+            gameObject.AddComponent(script.GetClass());
+        }
+        
+    }
+    */
+    
+}
