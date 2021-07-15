e1ef8ca5d1864a75dbd80156178e24073f7f3bbc
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-07-15 19:08:28 +0300

    Added alpha import tool

---
+++ Assets/Scripts/Importer/ImportSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utilities.Importer
+{
+    [CreateAssetMenu(fileName = "CustomImportSettings", menuName = "Custom Import Settings", order = 1010)]
+    public class ImportSettings : ScriptableObject
+    {
+        public string path;
+        //public static ImportSettings Instance;
+
+        public bool doPostProcess;
+        
+        
+
+
+    }
+}
---
+++ Assets/Scripts/Importer/ImportUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities.Importer
+{
+    public static class ImportUtility
+    {
+        public static void Process(List<string> assets, string assetPath)
+        {
+            
+            foreach (var asset in assets)
+            {
+                Debug.Log($"asset {assetPath}");
+                
+            }
+        }
+    }
+}
