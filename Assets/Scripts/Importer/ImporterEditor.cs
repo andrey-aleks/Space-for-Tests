3c2159ee97da9efec821f0146a9b76d57e57ed84
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-07-16 14:37:30 +0300

    WIP little refactor importer

--- Assets/Scripts/Importer/ImporterEditor.cs
+++ Assets/Scripts/Importer/ImporterEditor.cs
@@ -23,7 +23,6 @@ namespace Utilities.Importer
             Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
             string commonPath = regex.Replace(path, "");
             string filename = path.Split('/').Last().Split('.').First().Split('_')[1];
-            //Debug.Log("filename is " + filename);
             string currentDir = Environment.CurrentDirectory + "\\Assets\\";
 
             if (!AssetDatabase.IsValidFolder("Assets/Models/" + filename))
@@ -35,10 +34,6 @@ namespace Utilities.Importer
             string currentModelPath = "Assets\\Models\\" + filename + "\\" + "mesh_" + filename + ".fbx";
             AssetDatabase.ImportAsset(currentModelPath);
 
-/*            var importer = AssetImporter.GetAtPath(currentModelPath) as ModelImporter;
-            var success = importer.ExtractTextures("Assets/Textures/");
-            if (success) Debug.Log("success");
-            */
             var importedModels = AssetDatabase.LoadAllAssetsAtPath(currentModelPath)
                 .Where(x => x.GetType() == typeof(Material));
             if (!AssetDatabase.IsValidFolder("Assets/Models/" + filename + "/Materials/"))
@@ -48,7 +43,6 @@ namespace Utilities.Importer
 
             foreach (var model in importedModels)
             {
-                Debug.Log(model.name);
                 var message = AssetDatabase.ExtractAsset(model,
                     "Assets/Models/" + filename + "/Materials/" + model.name + ".mat");
                 if (!string.IsNullOrEmpty(message))
@@ -57,9 +51,6 @@ namespace Utilities.Importer
                 }
             }
 
-            //"Custom/SimpleShader"
-            //Material material = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-            //AssetDatabase.CreateAsset(material, "Assets\\Materials\\" + "mat_" + filename + ".mat");
             Material material =
                 (Material) AssetDatabase.LoadMainAssetAtPath("Assets\\Models\\" + filename + "\\Materials\\" + "mat_" +
                                                              filename + ".mat");
@@ -72,43 +63,37 @@ namespace Utilities.Importer
                 AssetDatabase.CreateFolder("Assets/Models/" + filename, "Textures");
             }
 
-            Debug.Log("mat is " + material.name);
-
             foreach (var sourceTexturePath in Directory.GetFiles(texturePath))
             {
                 Match textureName = textureRegex.Match(sourceTexturePath);
-                Debug.Log(textureName);
                 string currentTexturePath = currentDir + "\\Models\\" + filename + "\\Textures\\" + textureName;
                 File.Copy(sourceTexturePath, currentTexturePath);
                 string projectTexturePath = "Assets\\Models\\" + filename + "\\Textures\\" + textureName;
                 AssetDatabase.ImportAsset(projectTexturePath);
-                Debug.Log("split is " + textureName.ToString().Split('_').Last().Split('.').First());
                 switch (textureName.ToString().Split('_').Last().Split('.').First())
                 {
                     case "BC":
                         material.SetTexture("_BaseMap",
                             (Texture) AssetDatabase.LoadMainAssetAtPath(projectTexturePath));
-                        //Debug.Log(nameForDebug + obj.name + " is set to " + material.name);
                         break;
                     case "N":
                         material.SetTexture("_BumpMap",
                             (Texture) AssetDatabase.LoadMainAssetAtPath(projectTexturePath));
-                        TextureImporter textureImporter = AssetImporter.GetAtPath(projectTexturePath) as TextureImporter;
+                        TextureImporter textureImporter =
+                            AssetImporter.GetAtPath(projectTexturePath) as TextureImporter;
                         textureImporter.textureType = TextureImporterType.NormalMap;
-                        //Debug.Log(nameForDebug + obj.name + " is set to " + material.name);
                         break;
                     case "M":
                         material.SetTexture("_MetallicGlossMap",
                             (Texture) AssetDatabase.LoadMainAssetAtPath(projectTexturePath));
-                        //Debug.Log(nameForDebug + obj.name + " is set to " + material.name);
                         break;
                     case "AO":
                         material.SetTexture("_OcclusionMap",
                             (Texture) AssetDatabase.LoadMainAssetAtPath(projectTexturePath));
-                        //Debug.Log(nameForDebug + obj.name + " is set to " + material.name);
                         break;
                 }
             }
+
             EditorApplication.ExecuteMenuItem("File/Save Project");
         }
     }
