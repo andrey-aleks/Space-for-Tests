621cf9fed70aa29b60ebf514e22b6f570f402842
Andrey Alekseev <andrey.aleks00@gmail.com>
2021-08-05 16:57:05 +0300

    Fix exporter refactor

--- Assets/Scripts/Tools/Exporter/Editor/ExporterEditor.cs
+++ Assets/Scripts/Tools/Exporter/Editor/ExporterEditor.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 
 namespace Exporter.Editor
 {
     public class ExporterEditor : EditorWindow
     {
-        private static readonly ExporterSettings Settings = ExporterSettings.Instance;
-
-        private static string NAME = "[ExporterEditor]: ";
+        private static readonly string NAME = "[ExporterEditor]: "; // name for debug
 
         [MenuItem("Assets/Custom Tools/Export")]
         public static void ShowWindow()
@@ -22,23 +16,21 @@ namespace Exporter.Editor
 
         private static void Init()
         {
-            var exportObject = Selection.activeObject;
-            var targetPath = EditorUtility.SaveFilePanel("Export as", "", exportObject.name + ".fbx", "fbx");
-            
+            var exportObject = Selection.activeObject; // get selected object
+            var sourcePath = ExporterUtility.GetFullSourcePath(exportObject);
+
+            var targetPath =
+                EditorUtility.SaveFilePanel("Export as", "", exportObject.name + ".fbx",
+                    "fbx"); // open dialog window for export and get target path from this
+
             if (!string.IsNullOrEmpty(targetPath))
             {
-                ExporterUtility.Export(GetFullSourcePath(exportObject), targetPath);
+                ExporterUtility.Export(sourcePath, targetPath);
             }
             else
             {
                 Debug.Log($"{NAME}wrong path");
             }
-        }
-
-        private static string GetFullSourcePath(Object asset)
-        {
-            var sourceFullPath = Environment.CurrentDirectory + @"\" + AssetDatabase.GetAssetPath(asset);
-            return sourceFullPath;
         }
     }
 }
